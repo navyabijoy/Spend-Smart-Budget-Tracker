c968671d223aa6846d2a50ea51e5b47cc5a75a0d
Navya Bijoy <navyabijoy14@gmail.com>
2025-04-19 02:42:38 +0530

    navbar and clerk set up

---
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { SignIn } from "@clerk/nextjs";
+
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center w-screen h-screen bg-black">
+      <SignIn fallbackRedirectUrl={'/dashboard'} />
+    </div>
+  );
+}
---
+++ components/Navbar.jsx
@@ -0,0 +1,144 @@
+"use client";
+
+import { useState } from "react";
+import {
+  Dialog,
+  DialogPanel,
+  Disclosure,
+  DisclosureButton,
+  DisclosurePanel,
+  Popover,
+  PopoverButton,
+  PopoverGroup,
+  PopoverPanel,
+} from "@headlessui/react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import { HiCreditCard } from "react-icons/hi2";
+import {
+  SignedIn,
+  SignedOut,
+  SignIn,
+  SignInButton,
+  SignUp,
+  UserButton,
+  useUser,
+} from "@clerk/nextjs";
+import Link from "next/link";
+
+export default function Example() {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+  const { user } = useUser();
+  return (
+    <header className="bg-white">
+      <nav
+        aria-label="Global"
+        className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
+      >
+        <div className="flex lg:flex-1">
+          <a href="#" className="-m-1.5 p-1.5">
+            <div className="flex items-center gap-1">
+              <HiCreditCard className="h-6 w-auto text-indigo-800" />
+              <p className="font-bold text-indigo-800">Spendee</p>
+            </div>
+          </a>
+        </div>
+
+        <SignedOut>
+          <div className="flex lg:hidden">
+            <button
+              type="button"
+              onClick={() => setMobileMenuOpen(true)}
+              className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+            >
+              <span className="sr-only">Open main menu</span>
+              <Bars3Icon aria-hidden="true" className="size-6" />
+            </button>
+          </div>
+
+          <PopoverGroup className="hidden lg:flex lg:gap-x-12">
+            <a href="#" className="text-sm/6 font-semibold text-gray-900">
+              Features
+            </a>
+            <a href="#" className="text-sm/6 font-semibold text-gray-900">
+              Marketplace
+            </a>
+            <a href="#" className="text-sm/6 font-semibold text-gray-900">
+              Company
+            </a>
+          </PopoverGroup>
+        </SignedOut>
+        <div className="hidden lg:flex lg:flex-1 lg:justify-end cursor-pointer gap-2">
+          <SignedOut>
+              <Link href="/sign-in" className="text-sm/6 font-semibold text-gray-900 cursor-pointer">
+                Sign in &rarr;
+              </Link>
+            </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </div>
+      </nav>
+      <SignedOut>
+        <Dialog
+          open={mobileMenuOpen}
+          onClose={setMobileMenuOpen}
+          className="lg:hidden"
+        >
+          <div className="fixed inset-0 z-10" />
+          <DialogPanel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+            <div className="flex items-center justify-between">
+              <a href="#" className="-m-1.5 p-1.5">
+                <div className="flex items-center gap-1">
+                  <HiCreditCard className="h-6 w-auto text-indigo-800" />
+                  <p className="font-bold text-indigo-800">Spendee</p>
+                </div>
+              </a>
+              <button
+                type="button"
+                onClick={() => setMobileMenuOpen(false)}
+                className="-m-2.5 rounded-md p-2.5 text-gray-700"
+              >
+                <span className="sr-only">Close menu</span>
+                <XMarkIcon aria-hidden="true" className="size-6" />
+              </button>
+            </div>
+            <div className="mt-6 flow-root">
+              <SignedOut>
+                <div className="-my-6 divide-y divide-gray-500/10">
+                  <div className="space-y-2 py-6">
+                    <a
+                      href="#"
+                      className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
+                    >
+                      Features
+                    </a>
+                    <a
+                      href="#"
+                      className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
+                    >
+                      Marketplace
+                    </a>
+                    <a
+                      href="#"
+                      className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
+                    >
+                      Company
+                    </a>
+                  </div>
+                  <div className="py-6">
+                    <a
+                      href="#"
+                      className="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
+                    >
+                      Log in
+                    </a>
+                  </div>
+                </div>
+              </SignedOut>
+            </div>
+          </DialogPanel>
+        </Dialog>
+      </SignedOut>
+    </header>
+  );
+}
